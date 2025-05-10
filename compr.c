81c39f956f356f67df3e2ea2307f5a18a60aa83e
AyushChakraborty <chakrabortyayush83@gmail.com>
2025-05-10 10:13:19 +0530

    add functions for compressing the file

--- compr.c
+++ compr.c
@@ -19,30 +19,153 @@ int *char_freqs(FILE *file) {
 
 
 typedef struct node {
-    int freq;
-    char c;
+    int freq;   
+
+    union type{
+        int leaf;
+        char c;
+    }type;
+
     struct node *left;
     struct node *right;
 }NODE;  //node for the huffman tree
+
+
+// void heapSort(int len, NODE **nodes[len], int non_null_len) {
+    
+// }
+
+void heapify(int len, NODE **nodes[len], int non_null_len, int start_index) {
+    //just heapifying one node down, globally with this function
+    int smallest = start_index;     
+    int left = 2*start_index + 1;
+    int right = 2*start_index + 2;
+
+    if (left < non_null_len && (*nodes)[left]->freq < (*nodes)[smallest]->freq) {
+        smallest = left;
+    }if (right < non_null_len && (*nodes)[right]->freq < (*nodes)[smallest]->freq) {
+        smallest = right;
+    }
+
+    if (smallest != start_index) {
+        NODE *temp = (*nodes)[start_index];
+        (*nodes)[start_index] = (*nodes)[smallest];
+        (*nodes)[smallest] = temp;
+
+        //start recursive heapify again from the index which was involved in the swap, to ensure
+        //that the global min heap property remains
+        heapify(len, nodes, non_null_len, smallest);
+    }
+}
+
+void build_min_heap(int len, NODE **nodes[len], int non_null_len) {
+    //the loop instructs each node from last parent to the first one to fix its own subtree
+    //by calling the heapify function
+    for (int i=(non_null_len-2)/2; i>=0; i--) {
+        heapify(len, nodes, non_null_len, i);
+    }
+}
+
+
+void extractMin(int len, NODE **nodes[len], int *non_null_len, NODE **selected_nodes) {
+    /*
+    function to extract/remove the first two min nodes, and heapify the remaining part of the
+    heap array each time
+
+    params:
+        len: int, total size of the array
+        nodes: pointer to array of pointer to NODE, the primary array 
+        non_null_len: pointer to length of actual heap
+        selected_nodes: array of pointer to NODE, the selected nodes populate this array
+    */
+
+    NODE *min_node_0 = (*nodes)[0];
+    NODE *min_node_1 = NULL;
+
+    //since first node is removed, we insert the last leaf there, set the index of the last
+    //leaf to be NULL
+    (*nodes)[0] = (*nodes)[(*non_null_len)-1];
+    (*nodes)[(*non_null_len)-1] = NULL;
+    (*non_null_len)--;         //length of heap has decreased by one
+    //int start_index = (*non_null_len-2)/2;
+ 
+    build_min_heap(len, nodes, *non_null_len);
+
+    min_node_1 = (*nodes)[0];
+
+    (*nodes)[0] = (*nodes)[(*non_null_len)-1];
+    (*nodes)[(*non_null_len)-1] = NULL;
+    (*non_null_len)--;       
+ 
+    build_min_heap(len, nodes, *non_null_len);
+
+    selected_nodes[0] = min_node_0;
+    selected_nodes[1] = min_node_1;
+}
+
+
+void insertNode(int len, NODE **nodes[len], int *non_null_len, NODE **selected_nodes) {
+    //merge the selected nodes
+
+    int freq_0 = selected_nodes[0]->freq;
+    int freq_1 = selected_nodes[1]->freq;
+
+    NODE *local_root = (NODE*)malloc(sizeof(NODE));
+    local_root->type.leaf = 0;          //its not a leaf so the character it holds does not matter
+    //local_root->c = 'a';
+    local_root->freq = freq_0 + freq_1;
+    
+    if (freq_0 > freq_1) {
+        local_root->left = selected_nodes[1];
+        local_root->right = selected_nodes[0];
+    }else {
+        local_root->left = selected_nodes[0];
+        local_root->right = selected_nodes[1];
+    }
+
+    //time to insert the local root in the heap tree
+    (*nodes)[*non_null_len] = local_root;
+    (*non_null_len)++;      //since a new node has been inserted
+    //int start_index = (*non_null_len-2)/2;
+
+    build_min_heap(len, nodes, *non_null_len);
+}
+
 
 NODE *huff_tree(int *freqs, int len) {
     //make nodes for all chars with non zero freq
     NODE *nodes[len];     //all the nodes of the non-0 chars will be at the start of the array
 
     for (int i=0; i<len; i++) {
-        nodes[i] = NULL;          //NULL init
+        nodes[i] = NULL;     
     }
     int count = 0;
     for (int i=0; i<len; i++) {
         if (freqs[i] != 0) {
             NODE *new = (NODE*)malloc(sizeof(NODE));
+            //new->leaf = 1;         //as initially all nodes are leaves
             new->freq = freqs[i];
-            new->c = (char)i;
+            new->type.c = (char)i;
             new->left = NULL;
             new->right = NULL;
-            nodes[count] = new;
+            nodes[count] = new; 
             count++;
         }
+    }
+
+    // iterative process of making the huffman tree
+    // loop goes on till only one node is left
+    int start_index;
+    NODE *selected_nodes[2];
+
+    build_min_heap(len, &nodes, count);   
+
+    while(count != 1) {
+        //start_index = (count-2)/2;
+
+        extractMin(len, &nodes, &count, selected_nodes);
+
+        insertNode(len, &nodes, &count, selected_nodes);
     }
 
     return nodes[0];       //eventually the first element will be the root of the huffman tree
@@ -52,8 +175,9 @@ NODE *huff_tree(int *freqs, int len) {
 int main() {
     FILE *file = fopen("texts/s1.txt", "r");
     int *freqs = char_freqs(file);
-    for (int i=0; i<255; i++) {
+    for (int i=0; i<256; i++) {
         printf("%d\n", freqs[i]);
     }
+    fclose(file);
     return 0;
 }
